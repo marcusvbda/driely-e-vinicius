52f44dd1270925b5d6ea9a49069641580867a4f3
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-06-28 17:49:46 -0300

    top bar

---
+++ components/TopBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="top-container">
+        <div class="sdn-topo">
+            <img src="/top.png" class="lg-show sm-hide" alt="top">
+            <img src="/top_small.png" class="lg-hide sm-show" alt="top">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    // 
+}
+</script>
+<style lang="scss">
+.top-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;  
+    background-repeat: no-repeat;
+    .sdn-topo {
+        padding-top: 55px;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-<div>
-    LAYOUT AQUI
-    <Nuxt />
+<div class="container">
+   <div class="main">
+        <TopBar />
+        <Nuxt />
+   </div>
 </div>
 </template>
 <script>
@@ -9,3 +11,48 @@ export default {
     // 
 }
 </script>
+<style lang="scss">
+body {
+    background: url('static/background-lovelights.jpg') no-repeat top center;
+    color: #fff;
+    .container {
+        padding-bottom: 60px;
+        margin-top: 100px;
+    }
+
+    @media screen and (min-width: 972px) {
+        .lg-show {
+            display: block;
+        }
+
+        .lg-hide {
+            display: none;
+        }
+        
+        .sm-show {
+            display: none;
+        }
+
+        .sm-hide {
+            display: block;
+        }
+    }
+    @media screen and (max-width: 971px) {
+        .lg-show {
+            display: none;
+        }
+
+        .lg-hide {
+            display: block;
+        }
+        
+        .sm-show {
+            display: block;
+        }
+
+        .sm-hide {
+            display: none;
+        }
+    }
+}
+</style>
